29b290ef50dd8217b8fed9993c9daa1298f5f759
huaium <huaium233@gmail.com>
2025-08-14 16:59:15 +0800

    Fix version scheme logic

--- setup.py
+++ setup.py
@@ -6,9 +6,8 @@ from setuptools_scm import ScmVersion
 
 
 def custom_version(version: ScmVersion) -> str:
-    from setuptools_scm.version import guess_next_version
-
-    return version.format_next_version(guess_next_version, "{guessed}")
+    # Make the version the same as the tag
+    return version.format_with("{tag}")
 
 
 setup(use_scm_version={"version_scheme": custom_version})
